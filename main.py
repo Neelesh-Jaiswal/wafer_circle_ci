9a9a423d099c48fa320a1c5d86a4cd54e3b56da7
Neelesh-Jaiswal <nikhilneelesh1@gmail.com>
2021-11-21 12:15:42 +0530

    tetsing agan

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "Flask app is running. DOING SOME DUMMY CHANGES"
+    return "Flask app is running. DOING SOME DUMMY CHANGES.testing"
 
 port = int(os.getenv("PORT", 5001))
 
